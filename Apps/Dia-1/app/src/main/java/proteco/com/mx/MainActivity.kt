886a6fa35dd3159fdc8e07392f06e0fd2de56133
JohnKun136NVCP <73410627+JohnKun136NVCP@users.noreply.github.com>
2023-06-07 16:51:52 -0600

    [MAIN][FEATURE][INSTRUCTORES] Se agregó descripción para la segunda imagen y también boton que cambia de color y redirige a la una página de internet

--- Apps/Dia-1/app/src/main/java/proteco/com/mx/MainActivity.kt
+++ Apps/Dia-1/app/src/main/java/proteco/com/mx/MainActivity.kt
@@ -1,8 +1,11 @@
 package proteco.com.mx
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +15,23 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this,"OnCreate",Toast.LENGTH_SHORT).show()
 
     }
-    //ImageBotton
+    //ImageBotton poner funcionalidades a los botones.
     fun enviarMesaje(view: View){
         val texto = view.contentDescription.toString()
-        if(texto=="quintilliza"){
-            Toast.makeText(this,"Hey hola",Toast.LENGTH_SHORT).show()
+        if(texto=="quintilliza" || texto=="五等分"){ //En el caso que el idioma cambie
+            Toast.makeText(this,"Hey Yahooo!",Toast.LENGTH_SHORT).show()
+        }else if (texto=="My hero academia"||texto=="僕のヘロアカデミア"){
+            Toast.makeText(this,"My hero Academia",Toast.LENGTH_SHORT).show()
+        }
+    }
+    //Redireccionar a internet y después cambiar el botón (Así funcionan los anuncios xd)
+    fun botonInternet(view: View){ //Funcionalidad del boton llamando a todos los atributos de view
+        val botonCambia = findViewById<Button>(R.id.boton1) //Buscar por un ID
+        botonCambia.setOnClickListener{ //Estar atento cuando de click
+            Toast.makeText(this,"Plus Ultra",Toast.LENGTH_LONG).show() //Muestra un mensaje
+            val i  = Intent(Intent.ACTION_VIEW, Uri.parse("https://heroaca.com")) //Abre la pagina
+            botonCambia.backgroundTintList = getColorStateList(android.R.color.holo_green_dark) //Cambia el color del boton
+            startActivity(i) //Cuando empieza la pantalla principal o la actividad
         }
     }
     //Ciclo de vida de una APP
